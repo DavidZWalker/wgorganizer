7aeb361987e5707f725c1e826f6d5113292ec0b3
DavidZWalker <47477187+DavidZWalker@users.noreply.github.com>
2021-11-06 12:21:23 +0100

    update package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Zack Walker",
+    name="WG-Organizer",
     version="0.0.1",
     author="Zack Walker",
     author_email="zackdavidwalker94@gmail.com",
